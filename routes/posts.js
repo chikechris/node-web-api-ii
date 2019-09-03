fb2fabe512a5c9a29aae1db2155f87ab1bc01fc0
chikechris <cchike2017@gmail.com>
2019-09-03 16:46:37 -0400

    Built ou the post requst

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const db = require('../data/db.js')
 router.use(express.json())
 
+// Get
 router.get('/', (req, res) => {
   db.find()
     .then(posts => {
@@ -33,6 +34,43 @@ router.get('/:id', (req, res) => {
         .status(500)
         .json({ error: 'The post information could not be retrieved.' })
     })
+})
+
+// Post
+router.post('/api/posts/:id/comments', (req, res) => {
+  const postId = req.params.id
+  const commentObject = req.body
+
+  db.findById(postId).then(posts => {
+    if (posts.length > 0) {
+      if (commentObject.text) {
+        db.insertComment(commentObject)
+          .then(result => {
+            db.findCommentById(result.id)
+              .then(result => {
+                res.status(201).json(result)
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          })
+          .catch(err => {
+            res.status(500).json({
+              error:
+                'There was an error while saving the comment to the database'
+            })
+          })
+      } else {
+        res
+          .status(400)
+          .json({ errorMessage: 'Please provide text for the comment.' })
+      }
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' })
+    }
+  })
 })
 
 module.exports = router
