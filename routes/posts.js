2d1d59b1a1aa82bf7494880318fe6e0266582b1f
chikechris <cchike2017@gmail.com>
2019-09-03 17:35:32 -0400

    built out put requset

--- routes/posts.js
+++ routes/posts.js
@@ -17,7 +17,8 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/api/post/:id', (req, res) => {
+// Get post
+router.get('/api/posts', (req, res) => {
   const postId = req.params.id
   db.findById(postId)
     .then(post => {
@@ -33,6 +34,56 @@ router.get('/api/post/:id', (req, res) => {
       res
         .status(500)
         .json({ error: 'The post information could not be retrieved.' })
+    })
+})
+
+// Get post  by id
+router.get('/api/posts/:id', (req, res) => {
+  const id = req.params.id
+
+  db.findById(id)
+    .then(result => {
+      console.log(result.length)
+      if (result.length > 0) {
+        res.status(201).json(result)
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' })
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' })
+    })
+})
+
+// GET request for comments using post id:
+router.get('/api/posts/:id/comments', (req, res) => {
+  const postId = req.params.id
+  console.log('id from comments request', postId)
+
+  db.findById(postId)
+    .then(posts => {
+      if (posts.length > 0) {
+        db.findPostComments(Number(postId))
+          .then(result => {
+            res.status(201).json(result)
+          })
+          .catch(err => {
+            res.status(500).json({
+              error: 'The comments information could not be retrieved.'
+            })
+          })
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' })
+      }
+    })
+    .catch(err => {
+      console.log(err)
     })
 })
 
@@ -97,6 +148,44 @@ router.delete('/api/posts/:id', (req, res) => {
     .catch(error => {
       console.log(error)
     })
+})
+
+// Put
+router.put('/api/posts/:id', (req, res) => {
+  const postId = req.params.id
+  const postUpdate = req.body
+
+  console.log(postUpdate)
+
+  db.findById(postId).then(result => {
+    if (result.length > 0) {
+      if (postUpdate.title && postUpdate.contents) {
+        db.update(postId, postUpdate)
+          .then(results => {
+            db.findById(postId)
+              .then(success => {
+                res.status(200).json(success)
+              })
+              .catch(err => {
+                console.log(err)
+              })
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: 'The post information could not be modified.' })
+          })
+      } else {
+        res.status(400).json({
+          errorMessage: 'Please provide title and contents for the post.'
+        })
+      }
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' })
+    }
+  })
 })
 
 module.exports = router
