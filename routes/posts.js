92740e7f3c4f59a46bd8f5edc594bf27a1a5d4e6
chikechris <cchike2017@gmail.com>
2019-09-03 17:45:13 -0400

    modified get requsts

--- routes/posts.js
+++ routes/posts.js
@@ -3,38 +3,20 @@ const router = express.Router()
 const db = require('../data/db.js')
 router.use(express.json())
 
-// Get
+//Router test on root
 router.get('/', (req, res) => {
-  db.find()
-    .then(posts => {
-      res.status(200).json(posts)
-    })
-    .catch(err => {
-      res.status(500).json({
-        success: false,
-        message: 'The post information could not be retrieved.'
-      })
-    })
+  res.status(200).json({ message: 'Success!!!' })
 })
 
-// Get post
+// Get posts
 router.get('/api/posts', (req, res) => {
-  const postId = req.params.id
-  db.findById(postId)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post)
-      } else {
-        res
-          .status(404)
-          .json({ message: 'The post with the specified ID does not exist.' })
-      }
-    })
-    .catch(err => {
+  db.find()
+    .then(result => res.status(201).json(result))
+    .catch(error =>
       res
         .status(500)
-        .json({ error: 'The post information could not be retrieved.' })
-    })
+        .json({ error: 'The posts information could not be retrieved.' })
+    )
 })
 
 // Get post  by id
